a7ce9260d6c93421622f8cf1b6acd9fffc52aeba
sepidsoroush <s.soroush2012@gmail.com>
2024-10-20 19:44:23 +0300

    Initial Commit

---
+++ SwiftMapApp/SwiftMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMapAppApp.swift
+//  SwiftMapApp
+//
+//  Created by Sepideh Soroush on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
