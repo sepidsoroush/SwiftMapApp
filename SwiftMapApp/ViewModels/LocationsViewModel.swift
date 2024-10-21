eaa3e6b3a0452c2ac5186ba6142bfb32b65c231c
sepidsoroush <s.soroush2012@gmail.com>
2024-10-21 17:25:51 +0300

    add Location Preview cards with asymmetric Transitions

--- SwiftMapApp/ViewModels/LocationsViewModel.swift
+++ SwiftMapApp/ViewModels/LocationsViewModel.swift
@@ -54,4 +54,21 @@ class LocationsViewModel: ObservableObject {
             showLocationsList = false
         }
     }
+    
+    func nextButtonPressed() {
+        guard let currentIndex = locations.firstIndex(where: {$0 == mapLocation}) else {
+            print("Couldn't find current index in the locations array")
+            return
+        }
+        
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            guard let firstLocation = locations.first else {return}
+            showNextLocation(nextLocation: firstLocation)
+            return
+        }
+        
+        let nextLocation = locations[nextIndex]
+        showNextLocation(nextLocation: nextLocation)
+    }
 }
